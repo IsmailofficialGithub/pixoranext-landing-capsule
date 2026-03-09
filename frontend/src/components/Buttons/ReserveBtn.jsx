41a91e4ed834a6e806d39eabda47b648dd7aa9ab
Ismail abbasi <ismail.official295@gmail.com>
2026-03-09 05:48:02 +0500

    make whole Contact and Menu pills clickable for email

--- frontend/src/components/Buttons/ReserveBtn.jsx
+++ frontend/src/components/Buttons/ReserveBtn.jsx
@@ -3,14 +3,14 @@ import AnimateBtn from "./AnimateBtn";
 
 const ReserveBtn = () => {
     return (
-        <div className="relative z-49">
-            <div className="absolute right-6 top-[2vw] w-[8.5vw] bg-[#f4efe7] px-1 py-1 flex justify-end items-center rounded-4xl gap-2">
+        <a href="mailto:info@pixoranest.com" className="relative z-[100] cursor-pointer group">
+            <div className="absolute right-6 top-[2vw] w-fit bg-[#f4efe7] px-4 py-1 flex justify-end items-center rounded-4xl gap-2 hover:scale-105 transition-all duration-300">
                 <AnimateBtn btnName="Contact" href="mailto:info@pixoranest.com"/>
-
 
                 <MdArrowOutward className="bg-[#2a2725] text-[#b3a694] w-[2.5vw] h-auto rounded-full p-1" />
             </div>
-        </div>
+        </a>
+
     )
 }
 
